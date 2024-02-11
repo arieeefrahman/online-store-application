181d4dc6ca4520980ead22a752955bc6b1289ef8
Arief Rahman <ariefrahman01@mail.ugm.ac.id>
2024-02-11 18:31:07 +0700

    feat: create user models

---
+++ models/userModel.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
